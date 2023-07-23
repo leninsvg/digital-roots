46d91cf840335c647bbd040e74e6cc29375ad4ca
leninsvg <lenin@aktiun.com>
2023-07-22 22:34:13 -0500

    Add enum serializer for swagger ui

--- DigitalRoots/DigitalRoots.Api/Program.cs
+++ DigitalRoots/DigitalRoots.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using DigitalRoots.Api;
 using DigitalRoots.Business.Services;
 using DigitalRoots.Persistence;
@@ -17,7 +18,11 @@ builder.Services.AddScoped<IPetRepository, PetRepository>();
 builder.Services.AddScoped<IPersonService, PersonService>();
 builder.Services.AddScoped<IPetService, PetService>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
