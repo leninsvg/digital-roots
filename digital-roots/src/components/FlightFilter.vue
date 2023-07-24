4f323c61747532fd3547b28115c0f485d91a0f29
leninsvg <lenin@aktiun.com>
2023-07-24 16:14:14 -0500

    Hide actions date filter

--- digital-roots/src/components/FlightFilter.vue
+++ digital-roots/src/components/FlightFilter.vue
@@ -3,6 +3,7 @@ import { ref }         from 'vue';
 import router          from '@/router';
 import moment          from 'moment';
 import { useAppStore } from '@/store/app';
+import { tr }          from 'vuetify/locale';
 
 const originCity = ref('');
 const destinationCity = ref('');
@@ -37,8 +38,8 @@ const search = () => {
               :items="cities.filter(x => x !== originCity)"></v-select>
   </div>
   <div class="d-flex text-center">
-    <v-date-picker class="ma-4" v-model="startDate"></v-date-picker>
-    <v-date-picker class="ma-4" v-model="returnDate"></v-date-picker>
+    <v-date-picker class="ma-4" v-model="startDate" :hide-actions="true"></v-date-picker>
+    <v-date-picker class="ma-4" v-model="returnDate" :hide-actions="true"></v-date-picker>
   </div>
 </template>
 
